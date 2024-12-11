a1bb2e2ce8c0f1832bf82c8d7eeb9cd1d870ba19
ismael <spacingismael@gmail.com>
2024-12-11 22:07:54 +0100

    rest of tasks

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 
 app.use('/', routes);
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 module.exports = app;
