25137f2c188d9f33eb219dacc9408ad21cfb95f2
ismael <spacingismael@gmail.com>
2024-12-11 15:47:17 +0100

    task mongodb utils

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,36 @@
-#!/usr/bin/node
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+class DBClient {
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (err) {
+        console.log(err.message);
+        this.db = false;
+      } else {
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      }
+    });
+  }
+
+  isAlive() {
+    return Boolean(this.db);
+  }
+
+  async nbUsers() {
+    return this.usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    return this.filesCollection.countDocuments();
+  }
+}
+
+const dBClient = new DBClient();
+export default dBClient;
