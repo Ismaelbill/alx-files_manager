b1236819048685b652ac7471baad100f17fce368
ismael <spacingismael@gmail.com>
2024-12-11 17:12:22 +0100

    task 2. First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(request, response) {
+    const redisstatus = redisClient.isAlive();
+    const dbstatus = dbClient.isAlive();
+    response.status(200).send({ redis: redisstatus, db: dbstatus });
+  }
+
+  static async getStats(request, response) {
+    const nbUsers = await dbClient.nbUsers();
+    const nbFiles = await dbClient.nbFiles();
+    response.status(200).send({ users: nbUsers, files: nbFiles });
+  }
+}
+module.exports = AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+
+const router = Router();
+
+// routes
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
